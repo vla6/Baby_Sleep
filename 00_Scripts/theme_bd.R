ea35c7ebac9f9003b6f78a254bb48a7b5087c7d8
Valerie Carey <vla6@cornelledu>
2019-03-04 13:51:45 -0500

    Commonly used theme elements for plots

---
+++ 00_Scripts/theme_bd.R
@@ -0,0 +1,7 @@
+theme_bd <- theme_minimal() +
+  theme(plot.title = element_text(size=20),
+        axis.title.x = element_text(size=18),
+        axis.title.y= element_text(size=18),
+        axis.text.x  = element_text(size=14),
+        axis.text.y  = element_text(size=14),
+        legend.text = element_text(size=14)) 
